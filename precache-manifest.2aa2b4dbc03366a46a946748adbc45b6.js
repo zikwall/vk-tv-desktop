37080985611a615fee674351422bbfbe0992e6e6
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-10-17 15:08:22 +0300

    Updates

--- precache-manifest.2aa2b4dbc03366a46a946748adbc45b6.js
+++ precache-manifest.2aa2b4dbc03366a46a946748adbc45b6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "23dc37c99a6689268e93664fd68eeae3",
+    "revision": "b55a67529b88488c20e1bb03b85dc095",
     "url": "/vk-tv-desktop/index.html"
   },
   {
-    "revision": "7bac6ed32a11fbab7aad",
+    "revision": "f5b53fc4aea54f72b36b",
     "url": "/vk-tv-desktop/static/css/main.f27efc11.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vk-tv-desktop/static/js/2.41d91ece.chunk.js"
   },
   {
-    "revision": "7bac6ed32a11fbab7aad",
-    "url": "/vk-tv-desktop/static/js/main.d0d83c2f.chunk.js"
+    "revision": "f5b53fc4aea54f72b36b",
+    "url": "/vk-tv-desktop/static/js/main.895b1d73.chunk.js"
   },
   {
     "revision": "74a95159ed98bedab615",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk-tv-desktop/precache-manifest.25a69ec4b3f9aa00e22572874c114082.js"
+  "/vk-tv-desktop/precache-manifest.2aa2b4dbc03366a46a946748adbc45b6.js"
 );
 
 self.addEventListener('message', (event) => {
