5c5d3848181753405a0eb3ed9c6bc0ee36b9e925
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-10-18 16:44:15 +0300

    :art: Add channel logo animation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Aside } from "../../containers/aside";
+import styled, { keyframes } from 'styled-components';
+import { rubberBand } from 'react-animations';
 
 const Header = () => {
+    const rubberBandAnimation = keyframes`${rubberBand}`;
+    const RubberBandDiv = styled.div`
+        animation: 1s ${rubberBandAnimation};
+    `;
+
     return (
         <header id="site-header" className="site-header mb-1">
             <div className="master-container-fluid header-inner">
@@ -9,14 +16,16 @@ const Header = () => {
                     <div className="col-2 col-md-4 d-flex align-items-center">
                     </div>
                     <div className="col-auto col-md-4 align-items-center justify-content-center d-none d-md-flex">
-                        <div className="brand" id="channel-name">
-                            <a className="brand d-flex align-items-center" href="/">
+                        <RubberBandDiv>
+                            <div className="brand">
+                                <a className="brand d-flex align-items-center" href="/">
                                     <span className="adonis-icon mr-md-2 color-dark mr-1 icon-5x">
                                         <img style={{width: '26px'}} src="https://cdn.limehd.tv/images/playlist_1channel.png" />
                                     </span>
-                                <strong className="p-1 fs-6 fs-lg-8">Росиия 24</strong>
-                            </a>
-                        </div>
+                                    <strong className="p-1 fs-6 fs-lg-8">Росиия 24</strong>
+                                </a>
+                            </div>
+                        </RubberBandDiv>
                     </div>
                     <div className="col-auto d-flex justify-content-end justify-content-lg-end align-items-center navbar-secondary ml-auto">
                         <div className="mr-2">
