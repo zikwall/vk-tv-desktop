8bae340825487fa4c5575800e39d5f0679c50d1a
zikwall <andrey.kapitonov.96@gmail.com>
2019-10-31 21:57:23 +0300

    Updates

---
+++ precache-manifest.b134f0715a7e5e009cd8603d18e8dd87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd90faaa7aab98e30178a153aad356d3",
+    "url": "/vk-tv-desktop/index.html"
+  },
+  {
+    "revision": "caed6b3f8ee83a7e7f73",
+    "url": "/vk-tv-desktop/static/css/2.5d7f2840.chunk.css"
+  },
+  {
+    "revision": "4cc6d2fcb884e71dd047",
+    "url": "/vk-tv-desktop/static/css/main.f029946c.chunk.css"
+  },
+  {
+    "revision": "caed6b3f8ee83a7e7f73",
+    "url": "/vk-tv-desktop/static/js/2.058b4dd0.chunk.js"
+  },
+  {
+    "revision": "4cc6d2fcb884e71dd047",
+    "url": "/vk-tv-desktop/static/js/main.51d1392f.chunk.js"
+  },
+  {
+    "revision": "74a95159ed98bedab615",
+    "url": "/vk-tv-desktop/static/js/runtime-main.84069721.js"
+  },
+  {
+    "revision": "f7b02ba0f07252a8e5a3171ea0dfa9cf",
+    "url": "/vk-tv-desktop/static/media/dev.f7b02ba0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk-tv-desktop/precache-manifest.26e63fdfee08b3d70cf5b6b4e78df7b0.js"
+  "/vk-tv-desktop/precache-manifest.b134f0715a7e5e009cd8603d18e8dd87.js"
 );
 
 self.addEventListener('message', (event) => {
