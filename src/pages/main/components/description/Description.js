2a06a2bcb2946f64b6eb408af10f27db0838d310
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-11-01 15:07:42 +0300

    :pencil2: working with description

--- src/pages/main/components/description/Description.js
+++ src/pages/main/components/description/Description.js
@@ -1,25 +1,51 @@
 import React from 'react';
+import {getChannelsPending, getSelectChannel} from "../../../../redux/reducers";
+import { connect } from "react-redux";
+import DescriptionPlaceholder from "./Placeholder";
 
-const Description = () => {
+const Description = (props) => {
+    let { channel, pending } = props;
+
+    let content = '';
+
+    if (pending === true) {
+        content = <DescriptionPlaceholder />;
+    } else {
+        content = (
+            <>
+                <h6 className="inactive-color">Информация о передаче</h6>
+                <h3 className="album-title">Первый канал</h3>
+                <div className="separator mb-4 mt-4">
+                    <span className="separator-md"></span>
+                </div>
+                <div className="about">
+                    <h4>Вечерний Галустян</h4>
+                    <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+                        himenaeos.
+                        Suspendisse faucibus sed dolor eget posuere.Sed id interdum urna. Nam ac elit a
+                        ante commodo tristique. Duis lacus urna, condimentum a vehicula a, hendrerit ac
+                        nisi Lorem ipsum dolor sit amet
+                        Vestibulum imperdiet nibh vel magna lacinia ultrices. Sed id interdum urna. Nam
+                        ac elit a ante commodo tristique.
+                    </p>
+                    <p>
+                        Url: { channel.url }
+                    </p>
+                </div>
+            </>
+        )
+    }
+
     return (
         <div className="album-top-box text-center text-md-left">
-            <h6 className="inactive-color">Информация о передаче</h6>
-            <h3 className="album-title">Первый канал</h3>
-            <div className="separator mb-4 mt-4">
-                <span className="separator-md"></span>
-            </div>
-            <div className="about">
-                <h4>Вечерний Галустян</h4>
-                <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-                    himenaeos.
-                    Suspendisse faucibus sed dolor eget posuere.Sed id interdum urna. Nam ac elit a
-                    ante commodo tristique. Duis lacus urna, condimentum a vehicula a, hendrerit ac
-                    nisi Lorem ipsum dolor sit amet
-                    Vestibulum imperdiet nibh vel magna lacinia ultrices. Sed id interdum urna. Nam
-                    ac elit a ante commodo tristique. </p>
-            </div>
+            { content }
         </div>
     );
 };
 
-export default Description;
+const mapStateToProps = state => ({
+    pending: getChannelsPending(state),
+    channel: getSelectChannel(state),
+});
+
+export default connect(mapStateToProps)(Description);
---
+++ src/pages/main/components/description/Placeholder.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { BulletList } from 'react-content-loader'
+
+const MyBulletListLoader = () => <BulletList />
+
+export default MyBulletListLoader;
