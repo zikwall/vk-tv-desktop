b72d763fa436d5afb53c1f45a05d89626ce7d62f
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-11-28 16:44:32 +0300

    :pencil2: Crack JW and AD

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -27,22 +27,39 @@ const Player = (props) => {
         mute: true
     }];
 
+    const onReady = (event) => {
+        const player = window.jwplayer('my-unique-id');
+        closeSnackbar(props.snack)
+    };
+
+    const onVideoLoad = (event) => {
+        closeSnackbar(props.snack)
+    };
+
     return (
         <div className="video-container player-responsive" style={{width: '100%'}}>
             <ReactJWPlayer
-                playerId='my-unique-id'
-                playerScript='https://cdn.jwplayer.com/libraries/HLcL3DNo.js'
+                playerId='my-unique-idxxxwsws'
+                playerScript='https://cdn.jwplayer.com/libraries/31C1vqmP.js'
                 playlist={playlist}
                 isAutoPlay={true}
+                onReady={ onReady }
                 onError={(e) => {
                     props.snack = enqueueSnackbar('Не удалось загрузить канал :(', {
                         variant: 'error',
                         persist: true,
                     });
                 }}
-
-                onReady={(e) => {
-                    closeSnackbar(props.snack)
+                onVideoLoad={ onVideoLoad }
+                advertising={{
+                    client:  "googima",
+                    schedule: {
+                        pre:{
+                            offset: "pre",
+                            tag: "https://pubads.g.doubleclick.net/gampad/live/ads?iu=/14008522/video-jwp&description_url=https%3A%2F%2Flookandcook.com&env=vp&impl=s&correlator=&tfcd=0&npa=0&gdfp_req=1&output=vast&sz=400x300|640x480|970x90&min_ad_duration=10000&max_ad_duration=15000&unviewed_position_start=1"
+                        }
+                    },
+                    vpaidmode: "insecure"
                 }}
             />
         </div>
